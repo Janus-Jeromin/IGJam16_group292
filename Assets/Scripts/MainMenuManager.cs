e461cd317804d721eef3a47043450ebd1cab6fac
Janus Jeromin <jjeromin@eyefactive.com>
2025-11-08 12:57:04 +0100

    Added credits and option menu and curse now is a player preference

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,24 +1,64 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject _loadOverlay;
-    
+    [SerializeField] private string _startLevel;
+    [SerializeField] private GameObject _optionsMenu;
+    [SerializeField] private GameObject _creditsMenu;
+    [SerializeField] private Slider _curseSlider;
+    [SerializeField] private Slider _volumeSlider;
+
+    private void Awake()
+    {
+        _loadOverlay.SetActive(false);
+        _optionsMenu.SetActive(false);
+        _creditsMenu.SetActive(false);
+        
+        _curseSlider.value = PlayerPrefs.GetFloat("Curse", 0.0f);
+        _volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1.0f);
+    }
+
     public void OnStartClicked()
     {
         _loadOverlay.SetActive(true);
-        SceneManager.LoadSceneAsync("Level1");
+        SceneManager.LoadSceneAsync(_startLevel);
         this.gameObject.SetActive(false);
+    }
+
+    public void OnOptionsClicked()
+    {
+        _optionsMenu.SetActive(true);
+    }
+
+    public void OnCurseSliderChanged()
+    {
+        PlayerPrefs.SetFloat("Curse", _curseSlider.value);
+        PlayerPrefs.Save();
+    }
+    
+    public void OnVolumeSliderChanged()
+    {
+        PlayerPrefs.SetFloat("Volume", _volumeSlider.value);
+        PlayerPrefs.Save();
+    }
+
+    public void OnCreditsClicked()
+    {
+        _creditsMenu.SetActive(true);
+    }
+
+    public void OnCloseSubmenuClicked()
+    {
+        _optionsMenu.SetActive(false);
+        _creditsMenu.SetActive(false);
     }
 
     public void OnQuitClicked()
     {
         Application.Quit();
-    }
-
-    private void Start()
-    {
-        _loadOverlay.SetActive(false);
     }
 }
