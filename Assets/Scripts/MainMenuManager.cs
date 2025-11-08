7b2c6842d0067179089543ca2f866f1bdf118920
Janus Jeromin <jjeromin@eyefactive.com>
2025-11-08 18:59:14 +0100

    Added music

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -4,14 +4,17 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Audio;
 
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject _loadOverlay;
     [SerializeField] private GameObject _main;
     [SerializeField] private Slider _curseSlider;
-    [SerializeField] private Slider _volumeSlider;
+    [SerializeField] private Slider _musicSlider;
+    [SerializeField] private Slider _sfxSlider;
     [SerializeField] private AudioSource _audioSourceVolumeChanged;
+    [SerializeField] private AudioMixer _audioMixer;
 
     private List<GameObject> _listSubmenus;
     private bool _wasAwaken = false;
@@ -22,7 +25,8 @@ public class MainMenuManager : MonoBehaviour
         _loadOverlay.SetActive(false);
         
         _curseSlider.value = PlayerPrefs.GetFloat("Curse", 0.0f);
-        _volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1.0f);
+        _musicSlider.value = PlayerPrefs.GetFloat("Volume Music", 1.0f);
+        _sfxSlider.value = PlayerPrefs.GetFloat("Volume SFX", 1.0f);
         _listSubmenus =  new List<GameObject>();
 
         _wasAwaken = true;
@@ -50,16 +54,30 @@ public class MainMenuManager : MonoBehaviour
         PlayerPrefs.Save();
     }
     
-    public void OnVolumeSliderChanged()
+    public void OnVolumeMusicSliderChanged()
     {
-        PlayerPrefs.SetFloat("Volume", _volumeSlider.value);
+        PlayerPrefs.SetFloat("Volume Music", _musicSlider.value);
+        PlayerPrefs.Save();
+        
+        _audioMixer.SetFloat("Volume Music", CalcAttenuationForVolume(_musicSlider.value));
+    }
+    
+    public void OnVolumeSFXSliderChanged()
+    {
+        PlayerPrefs.SetFloat("Volume SFX", _sfxSlider.value);
         PlayerPrefs.Save();
 
-        AudioListener.volume = _volumeSlider.value;
+        _audioMixer.SetFloat("Volume SFX", CalcAttenuationForVolume(_sfxSlider.value));
         
         // Do not play the sound before we even started
         if (_wasAwaken && !_audioSourceVolumeChanged.isPlaying) 
             _audioSourceVolumeChanged.Play();
+    }
+
+    private float CalcAttenuationForVolume(float volume)
+    {
+        // TODO explain the maths here. Something about perceived volume is logarithmic
+        return (float)Math.Log10(Math.Max(0.000001f, volume)) * 20;
     }
 
     public void OnCloseSubmenuClicked()
